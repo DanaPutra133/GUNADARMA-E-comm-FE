700892b56fbb63eb37eef20cbece6851b8d9b192
unknown <raflitrihanafi09@gmail.com>
2024-10-15 09:46:58 +0700

    ubah app

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite</h1>
+      <h1>rafli anj+++</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
