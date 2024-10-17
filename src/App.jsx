5aa2cb03f6eaf3ff75644a473ab8b61e96606022
unknown <pangeranchristianosiahaan1478@gmail.com>
2024-10-17 13:49:13 +0700

    error

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const a = ({}=>{
+
+})
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -20,7 +24,7 @@ function App() {
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
+        </button> 
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
