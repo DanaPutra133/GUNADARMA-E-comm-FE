18d688e0e6dbb263cea873af4f4eef8f81a6a82e
unknown <pangeranchristianosiahaan1478@gmail.com>
2024-10-26 16:23:19 +0700

    ngerubah posisi filter dibagian products

--- src/components/shopping-view/filter.jsx
+++ src/components/shopping-view/filter.jsx
@@ -7,7 +7,7 @@ import { Separator } from "../ui/separator";
 function ProductFilter({ filters, handleFilter }) {
   return (
     <div className="bg-background rounded-lg shadow-sm">
-      <div className="p-4 border-b">
+      <div className="p-5 border-b">
         <h2 className="text-lg font-extrabold">Filters</h2>
       </div>
       <div className="p-4 space-y-4">
@@ -15,7 +15,7 @@ function ProductFilter({ filters, handleFilter }) {
           <Fragment>
             <div>
               <h3 className="text-base font-bold">{keyItem}</h3>
-              <div className="grid gap-2 mt-2">
+              <div className="grid gap-4 mt-4">
                 {filterOptions[keyItem].map((option) => (
                   <Label className="flex font-medium items-center gap-2 ">
                     <Checkbox
