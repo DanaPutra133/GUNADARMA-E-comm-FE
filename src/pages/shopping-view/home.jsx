ce11a3e9f94fc5257445a76b3aefabfe0bb61cdd
unknown <pangeranchristianosiahaan1478@gmail.com>
2024-10-26 15:34:06 +0700

    ngerubah bagian user category

--- src/pages/shopping-view/home.jsx
+++ src/pages/shopping-view/home.jsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { PiPants } from "react-icons/pi";
 // import bannerOne from "../../assets/banner-1.webp";
 // import bannerTwo from "../../assets/banner-2.webp";
 // import bannerThree from "../../assets/banner-3.webp";
@@ -32,8 +33,8 @@ import ProductDetailsDialog from "@/components/shopping-view/product-details";
 import { getFeatureImages } from "@/store/common-slice";
 
 const categoriesWithIcon = [
-  { id: "men", label: "Men", icon: ShirtIcon },
-  { id: "women", label: "Women", icon: CloudLightning },
+  { id: "men", label: "Shirt", icon: ShirtIcon },
+  { id: "women", label: "Pants", icon: PiPants },
   { id: "kids", label: "Kids", icon: BabyIcon },
   { id: "accessories", label: "Accessories", icon: WatchIcon },
   { id: "footwear", label: "Footwear", icon: UmbrellaIcon },
@@ -184,7 +185,8 @@ function ShoppingHome() {
         </div>
       </section>
 
-      <section className="py-12 bg-gray-50">
+`dihapus ngilangin atribut category by brand`
+      {/* <section className="py-12 bg-gray-50">
         <div className="container mx-auto px-4">
           <h2 className="text-3xl font-bold text-center mb-8">Shop by Brand</h2>
           <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-4">
@@ -201,7 +203,7 @@ function ShoppingHome() {
             ))}
           </div>
         </div>
-      </section>
+      </section> */}
 
       <section className="py-12">
         <div className="container mx-auto px-4">
