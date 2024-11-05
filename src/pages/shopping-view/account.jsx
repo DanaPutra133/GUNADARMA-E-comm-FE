d1c97ea78ddb276e007891a29787fcf4a5749562
dana <danaputra1001@gmail.com>
2024-11-05 17:14:44 +0700

    fix bug profile gak nampil

--- src/pages/shopping-view/account.jsx
+++ src/pages/shopping-view/account.jsx
@@ -1,5 +1,9 @@
+//INI BUAT MASUK PROFILE USER
+
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-// import accImg from "../../assets/account.jpg";
+//nanti module ini ganti sama foto yang user upload aja
+import accImg from "../../assets/logo/logo.jpg";
+//import alamat sama order di bawah sini
 import Address from "@/components/shopping-view/address";
 import ShoppingOrders from "@/components/shopping-view/orders";
 
@@ -8,6 +12,7 @@ function ShoppingAccount() {
     <div className="flex flex-col">
       <div className="relative h-[300px] w-full overflow-hidden">
         <img
+        // taruh sini aja buat si img/ profile user nya
           src={accImg}
           className="h-full w-full object-cover object-center"
         />
@@ -16,6 +21,7 @@ function ShoppingAccount() {
         <div className="flex flex-col rounded-lg border bg-background p-6 shadow-sm">
           <Tabs defaultValue="orders">
             <TabsList>
+              {/* alamat dari order dan isi order */}
               <TabsTrigger value="orders">Orders</TabsTrigger>
               <TabsTrigger value="address">Address</TabsTrigger>
             </TabsList>
