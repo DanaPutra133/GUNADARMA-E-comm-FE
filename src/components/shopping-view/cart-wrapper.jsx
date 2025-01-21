ddf6492b92a61228615fe2779be21b6c64fc7058
dana <danaputra1001@gmail.com>
2025-01-21 20:54:12 +0700

    merubah semua harga menjadi IDR

--- src/components/shopping-view/cart-wrapper.jsx
+++ src/components/shopping-view/cart-wrapper.jsx
@@ -18,7 +18,8 @@ function UserCartWrapper({ cartItems, setOpenCartSheet }) {
           0
         )
       : 0;
-// menampilkan keranjang produk yang ditambahkan
+
+  // Menampilkan keranjang produk yang ditambahkan
   return (
     <SheetContent className="sm:max-w-md">
       <SheetHeader>
@@ -26,13 +27,33 @@ function UserCartWrapper({ cartItems, setOpenCartSheet }) {
       </SheetHeader>
       <div className="mt-8 space-y-4">
         {cartItems && cartItems.length > 0
-          ? cartItems.map((item) => <UserCartItemsContent cartItem={item} />)
-          : null}
+          ? cartItems.map((item) => (
+              <div key={item.productId} className="flex justify-between items-center">
+                <UserCartItemsContent cartItem={item} />
+                <div className="text-right">
+                  <span
+                    className={`${
+                      item?.salePrice > 0 ? "line-through" : ""
+                    } text-lg font-semibold text-primary`}
+                  >
+                    Rp{new Intl.NumberFormat("id-ID").format(item?.price || 0)}
+                  </span>
+                  {item?.salePrice > 0 ? (
+                    <span className="text-lg font-bold block">
+                      Rp{new Intl.NumberFormat("id-ID").format(item?.salePrice || 0)}
+                    </span>
+                  ) : null}
+                </div>
+              </div>
+            ))
+          : <p className="text-center text-gray-500">Keranjang kosong.</p>}
       </div>
       <div className="mt-8 space-y-4">
         <div className="flex justify-between">
           <span className="font-bold">Total</span>
-          <span className="font-bold">Rp{totalCartAmount}</span>
+          <span className="font-bold">
+            Rp{new Intl.NumberFormat("id-ID").format(totalCartAmount || 0)}
+          </span>
         </div>
       </div>
       <Button
