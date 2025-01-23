b69f2bc3bf778f0d260303b44437392d9f5850b3
dana <danaputra1001@gmail.com>
2025-01-23 14:50:41 +0700

    dah fix

--- src/store/auth-slice/index.js
+++ src/store/auth-slice/index.js
@@ -12,7 +12,7 @@ export const registerUser = createAsyncThunk(
 
   async (formData) => {
     const response = await axios.post(
-      "https://tugas.aquafxca.my.id/api/auth/register",
+      "http://localhost:5004/api/auth/register",
       formData,
       {
         withCredentials: true,
@@ -28,7 +28,7 @@ export const loginUser = createAsyncThunk(
 
   async (formData) => {
     const response = await axios.post(
-      "https://tugas.aquafxca.my.id/api/auth/login",
+      "http://localhost:5004/api/auth/login",
       formData,
       {
         withCredentials: true,
@@ -44,7 +44,7 @@ export const logoutUser = createAsyncThunk(
 
   async () => {
     const response = await axios.post(
-      "https://tugas.aquafxca.my.id/api/auth/logout",
+      "http://localhost:5004/api/auth/logout",
       {},
       {
         withCredentials: true,
@@ -60,7 +60,7 @@ export const checkAuth = createAsyncThunk(
 
   async () => {
     const response = await axios.get(
-      "https://tugas.aquafxca.my.id/api/auth/check-auth",
+      "http://localhost:5004/api/auth/check-auth",
       {
         withCredentials: true,
         headers: {
